5fb09e7ac07bc0c340ec21ad295dc54de8526057
Francesc Castell Portella <56691800+FranckyCastell@users.noreply.github.com>
2024-02-06 20:58:26 +0100

    Update app.ino

--- app.ino
+++ app.ino
@@ -1,26 +1,25 @@
-#include <Servo.h> // IMPORTAM LA LLIBRERIA SERVO
-
-// IMPORTAM LES LLIBRERIES PER A LA PANTALLA OLED
-#include <Wire.sh>
+#include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128 // DEFINIM ANCHURA DE LA PANTALLA EN PIXELS
-#define SCREEN_HEIGHT 64 // DEFINIM ALTURA DE LA PANTALLA EN PIXELS
+#define __DEBUG__
+#define ANCHO_PANTALLA 128 // ancho pantalla OLED
+#define ALTO_PANTALLA 64   // alto pantalla OLED
 
 // DECLARACIONS PER A LA PANTALLA
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(ANCHO_PANTALLA, ALTO_PANTALLA, &Wire, -1);
 
 // CREACIO DELS SERVOS
 Servo Shorizontal; // CREACIO DE L'OBJECTE SERVO PER A L'EIX HORIZONTAL
 Servo Svertical;   // CREACIO DE L'OBJECTE SERVO PER A L'EIX VERTICAL
 
 // LDR PINS
-int ldr_01 = 33;       // PIN DRETA
-int ldr_02 = 35;       // PIN ESQUERRA
-int ldr_03 = 34;       // PIN BAIX
-int ldr_04 = 36;       // PIN DALT
-int Clean_Buffer = 39; // PIN PER A DEFINIR GROUD = 0
+int ldr_01 = 33;       // DEFINIM PIN DRETA
+int ldr_02 = 35;       // DEFINIM PIN ESQUERRA
+int ldr_03 = 34;       // DEFINIM PIN BAIX
+int ldr_04 = 36;       // DEFINIM PIN DALT
+int Clean_Buffer = 39; // DEFINIM PIN PER A DEFINIR GROUD = 0
 
 // VARIABLES DE LOS FOTORESISTENCIAS
 int rightLDR = 0;
@@ -31,36 +30,62 @@ int topLDR = 0;
 // DIFERENCIES ENTRE LES FOTORESISTENCIES
 int horizontalError = 0;
 int verticalError = 0;
+
+// POSICIONS DELS SERVOMOTORS INICIALS
 int ShorizontalPos = 90;
 int SverticalPos = 90;
+
+// VARIABLES PER ELS ANGLES
+int acimut = 0;
+int inclinacio = 0;
+int posicion = 0;
 
 // PINS SERVOMOTORS
 void setup()
 {
+#ifdef __DEBUG__
     Serial.begin(9600);
-    Shorizontal.attach(25); // SERVOMOTOR 1
-    Svertical.attach(26);   // SERVOMOTOR 2
+    delay(100);
+    Serial.println("INICIANT PANTALL OLED");
+#endif
+
+    // Iniciar pantalla OLED en la dirección 0x3C
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+#ifdef __DEBUG__
+        Serial.println("NO S'HA TROBAT LA PANTALLA OLED");
+#endif
+        while (true)
+            ;
+    }
+    Shorizontal.attach(25); // DEFINIM SERVOMOTOR 1 PIN
+    Svertical.attach(26);   // DEFINIM SERVOMOTOR 2 PIN
 }
 
 void loop()
 {
     // OLED CONSTANTS
-    display.clearDisplay();      // BORRAMOS CONTENIDO DE LA PANTALLA OLED
-    display.setTextSize(1);      // DEFINIMOS TAMAÑO DEL TEXTO QUE SE VA A MOSTRAR EL 1 ES EL TAMAÑO MINIMO
-    display.setTextColor(WHITE); // COLOR DEL TEXTO
+    display.clearDisplay();              // BORRAMOS CONTENIDO DE LA PANTALLA OLED
+    display.setTextSize(1);              // DEFINIMOS TAMAÑO DEL TEXTO QUE SE VA A MOSTRAR EL 1 ES EL TAMAÑO MINIMO
+    display.setTextColor(SSD1306_WHITE); // COLOR DEL TEXTO
     // OLED CONSTANTS
 
     // OLED LOOP
-    display.setCursor(0, 10);        // MOVEM EL CURSOS A LA POSICIO (0, 10) DE LA PANTALLA OLED
-    acimut = Shorizontal.read();     // LLEGIM VALORS DEL SERVO HORIZONTAL
-    display.print("ACIMUT", acimut); // IMPRIMIM L'ACIMUT A LA QUE ES TROBA EL SERVO EN AQUEST MOMENT
+    display.setCursor(25, 10); // MOVEM EL CURSOS A LA POSICIO (0, 10) DE LA PANTALLA OLED // COLUMNA , FILERA
+    display.print("~~~ PAMFER ~~~");
 
-    display.setCursor(0, 20);                    // MOVEM EL CURSOS A LA POSICIO (0, 20) DE LA PANTALLA OLED
-    inclinacio = Svertical.read();               // LLEGIM VALORS DEL SERVO VERTICAL
-    display.println("INCLINACIO: ", inclinacio); // IMPRIMIM LA INCLINACIÓ A LA QUE ES TROBA EL SERVO EN AQUEST MOMENT
+    display.setCursor(20, 30);   // MOVEM EL CURSOS A LA POSICIO (0, 10) DE LA PANTALLA OLED // COLUMNA , FILERA
+    acimut = Shorizontal.read(); // LLEGIM VALORS DEL SERVO HORIZONTAL
+    display.print("- ACIMUT: ");
+    display.print(acimut); // IMPRIMIM L'ACIMUT A LA QUE ES TROBA EL SERVO EN AQUEST MOMENT
+
+    display.setCursor(20, 40);     // MOVEM EL CURSOS A LA POSICIO (0, 20) DE LA PANTALLA OLED // COLUMNA , FILERA
+    inclinacio = Svertical.read(); // LLEGIM VALORS DEL SERVO VERTICAL
+    display.print("- INCLINACIO: ");
+    display.println(inclinacio); // IMPRIMIM LA INCLINACIÓ A LA QUE ES TROBA EL SERVO EN AQUEST MOMENT
 
     display.display(); // ACTUALIZAM LA PANTALLA AMB ELS DATOS ANTERIORS
-    // OLED LOOPs
+    // OLED LOOP
 
     // LDR PRINT
     analogRead(Clean_Buffer); // Ponemos el Buffer a 0
@@ -89,7 +114,7 @@ void loop()
     // SERVO PRINT
     posicion = Shorizontal.read();
     Serial.print(" SERVO HORIZONTAL: ");
-    Serial.println(posicion);
+    Serial.print(posicion);
 
     posicion = Svertical.read();
     Serial.print(" SERVO VERTICAL: ");
@@ -132,6 +157,6 @@ void loop()
         SverticalPos = constrain(SverticalPos, 10, 120);
         Svertical.write(SverticalPos);
     }
-    delay(100); // 25
+    delay(1000); // 25
                 // LOGICA
 }
